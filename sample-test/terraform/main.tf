3b495c542cfdcbd4c2f8215a269a4692958c0fca
suraj <surajbele51@gmail.com>
2026-05-15 17:57:37 +0530

    eks cluster using terraform

---
+++ sample-test/terraform/main.tf
@@ -0,0 +1,51 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.31"
+
+  # Networking
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Public access endpoint (for kubectl access from your machine)
+  cluster_endpoint_public_access = true
+
+  # Managed Node Groups
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 5
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+
+  # Enable cluster creator admin permissions automatically
+  enable_cluster_creator_admin_permissions = true
+}
---
+++ sample-test/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  default = "vpc-0401621fe5cf7db26"
+}
+variable "subnet_id_az1" {
+  default = subnet-064eadf67c4d0a06d
+}
+variable "subnet_id_az2" {
+  
+}
