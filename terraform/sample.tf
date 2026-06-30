09d4bf13285f317b5d2678c9f55aa1a60d45fee7
Pooja Devops <poojagangasagar@email.com>
2026-06-30 16:34:58 +0530

    sg is added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "myinstance" {
     ami = "ami-0aba19e56f3eaec05"
     instance_type = "t3.micro"
     key_name = "id_rsa"
-    vpc_security_group_ids = ["vpc-08a70fa338e668577",]
+    vpc_security_group_ids = ["sg-0eaa53a76d9c9fa6c",]
     tags = {
         name = "pooja"
         Name = "myinstance"
